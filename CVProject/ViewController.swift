876ff1acb4aa3b1dc282b87e8207832c4e5c9bda
tubaaNur <yesiltastubanur@gmail.com>
2023-07-09 19:38:50 +0300

    Initial Commit

---
+++ CVProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CVProject
+//
+//  Created by Tuba Nur  on 9.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
